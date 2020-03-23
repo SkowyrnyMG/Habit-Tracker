be3e2ce9165981885a014effa086afa42f998186
Mateusz <m.gruzla@hotmail.com>
2020-03-23 23:55:51 +0100

    Check if user is logged in function added. Habbit List added. Posibility to add multiple habits added. Bug with saving data to multiple docs fixed.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 import { carousel } from './carousel';
 import { nameDays, renderDays, progressBar } from './daysCounter';
 
+const app = firebase.app();
+const db = firebase.firestore();
+const auth = firebase.auth();
+const DEFAULT_DOC = '0000000';
+const habbitListBtn = document.querySelector('.habbit-placeholder__button');
+const habbitListHolder = document.querySelector('.habbit-placeholder__habbit-list');
+
 const changeColor = (statusData, month, day, target) => {
   if (Number(statusData) === 0 || Number(statusData) === 2) {
     habbitDataBase[month][day][3] = 1;
@@ -12,14 +19,10 @@ const changeColor = (statusData, month, day, target) => {
 };
 
 const habbitDataBase = renderDays();
-
 const checkTargetStatus = (user, curHabbit) => {
   const callendar = document.querySelector('.habbit-proggres__calendar');
 
   callendar.addEventListener('click', e => {
-    const app = firebase.app();
-    const db = firebase.firestore();
-
     const target = e.target;
     if (target.className != 'circle') {
       return;
@@ -44,8 +47,6 @@ const checkTargetStatus = (user, curHabbit) => {
 };
 
 const getHabitStatus = (user, curHabbit) => {
-  const app = firebase.app();
-  const db = firebase.firestore();
   const currentHabbit = db.collection(user).doc(curHabbit);
 
   const habbitDisplay = document.querySelector('.current-habbit');
@@ -77,11 +78,135 @@ const getHabitStatus = (user, curHabbit) => {
 };
 
 const checkIfUserLogged = () => {
-  const user = firebase.auth().currentUser;
-  if (user) {
-    logInWindow.classList.add('login-box-hide');
-  }
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log(user);
+      console.log(user.uid);
+      habbitController(user.uid);
+      renderHabbitList(user.uid);
+      logInWindow.classList.remove('login-box-active');
+    } else {
+      logInWindow.classList.remove('login-box-active');
+      logInWindow.classList.add('login-box-active');
+    }
+  });
 };
+
+const collectionSize = (uid, habbit) => {
+  db.collection(uid)
+    .doc(DEFAULT_DOC)
+    .get()
+    .then(doc => {
+      const data = doc.data();
+      return data.lastHabbit;
+    })
+    .then(lastHabbit => {
+      db.collection(uid)
+        .get()
+        .then(snap => {
+          const data = lastHabbit ? lastHabbit : snap.docs[snap.docs.length - 1].id;
+          return [snap.size, data];
+        })
+        .then(([size, data]) => {
+          if (size > 1) {
+            console.log('it has a doc');
+
+            getHabitStatus(uid, data);
+            checkTargetStatus(uid, data);
+          } else {
+            console.log('it does not have a doc');
+
+            db.collection(uid)
+              .doc(`${habbit}`)
+              .set({});
+          }
+        });
+    });
+};
+
+const renderHabbitList = userID => {
+  db.collection(userID)
+    .get()
+    .then(doc => {
+      doc.docs.forEach(doc => {
+        if (doc.id === DEFAULT_DOC) {
+          return;
+        } else {
+          const habbitListItem = document.createElement('li');
+          habbitListItem.className = 'habbit-placeholder__habbit-list__item';
+          habbitListItem.value = `${doc.id}`;
+          console.log(doc);
+          habbitListItem.innerHTML = doc.id;
+          habbitListHolder.appendChild(habbitListItem);
+        }
+      });
+    });
+};
+
+const habbitController = uid => {
+  const formHabbit = document.querySelector('.habbit-form');
+  const inputHabbit = document.getElementById('input-habbit');
+
+  db.collection(uid)
+    .get()
+    .then(snap => {
+      return snap.size;
+    })
+    .then(size => {
+      const callendar = document.querySelector('.habbit-proggres');
+      const errorInputArrow = document.querySelector('.error-arrow');
+      const errorInputInfo = document.querySelector('.error-info');
+      const habbitDisplay = document.querySelector('.current-habbit');
+
+      if (size > 1) {
+        collectionSize(uid);
+      } else {
+        callendar.classList.add('callendar-unactive');
+        errorInputArrow.classList.add('active');
+        errorInputInfo.classList.add('active');
+      }
+      formHabbit.addEventListener('submit', e => {
+        e.preventDefault();
+        callendar.classList.remove('callendar-unactive');
+        errorInputArrow.classList.remove('active');
+        errorInputInfo.classList.remove('active');
+        db.collection(uid)
+          .doc(`${inputHabbit.value}`)
+          .set({});
+        collectionSize(uid, inputHabbit.value);
+        habbitDisplay.innerHTML = inputHabbit.value;
+        db.collection(uid)
+          .doc(DEFAULT_DOC)
+          .set({
+            lastHabbit: inputHabbit.value
+          })
+          .then(() => {
+            location.reload();
+          });
+      });
+    });
+};
+
+const googleLoginAndGetData = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+
+    .then(result => {
+      const uid = result.user.uid;
+      if (!db.collection(uid).doc(DEFAULT_DOC)) {
+        db.collection(uid)
+          .doc(DEFAULT_DOC)
+          .set({});
+      }
+    })
+    .catch(console.log);
+};
+
+habbitListBtn.addEventListener('click', () => {
+  habbitListHolder.classList.toggle('habbit-list-active');
+});
 
 const logInWindow = document.querySelector('.login-box');
 const googleLoginBtn = document.querySelector('.btn-login');
@@ -91,106 +216,6 @@ checkIfUserLogged();
 googleLoginBtn.addEventListener('click', () => {
   googleLoginAndGetData();
 });
-
-const googleLoginAndGetData = () => {
-  const formHabbit = document.querySelector('.habbit-form');
-  const inputHabbit = document.getElementById('input-habbit');
-
-  const app = firebase.app();
-  const db = firebase.firestore();
-  const provider = new firebase.auth.GoogleAuthProvider();
-  let user_ID;
-
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-
-    .then(result => {
-      const user = result.user;
-      const uid = result.user.uid;
-      user_ID = uid;
-      console.log(user);
-      db.collection(uid)
-        .doc('0000000')
-        .set({
-          lastHabbit: ''
-        });
-      return uid;
-    })
-
-    .then(uid => {
-      const collectionSize = (uid, habbit) => {
-        // db.collection(uid)
-        //   .doc('0000000')
-        //   .get()
-        //   .then(doc => {
-        //     const data = doc.data();
-        //     return data.lastHabbit;
-        //   }).then(lastHabbit => {})
-
-        db.collection(uid)
-          .get()
-          .then(snap => {
-            const data = snap.docs[snap.docs.length - 1].id;
-            return [snap.size, data];
-          })
-          .then(([size, data]) => {
-            if (size > 1) {
-              console.log('it has a doc');
-
-              getHabitStatus(uid, data);
-              checkTargetStatus(uid, data);
-            } else {
-              console.log('it does not have a doc');
-
-              db.collection(uid)
-                .doc(`${habbit}`)
-                .set({});
-            }
-          });
-      };
-
-      db.collection(uid)
-        .get()
-        .then(snap => {
-          return snap.size;
-        })
-        .then(size => {
-          const callendar = document.querySelector('.habbit-proggres');
-          const errorInputArrow = document.querySelector('.error-arrow');
-          const errorInputInfo = document.querySelector('.error-info');
-          const habbitDisplay = document.querySelector('.current-habbit');
-
-          if (size > 1) {
-            collectionSize(uid);
-          } else {
-            callendar.classList.add('callendar-unactive');
-            errorInputArrow.classList.add('active');
-            errorInputInfo.classList.add('active');
-          }
-          formHabbit.addEventListener('submit', e => {
-            e.preventDefault();
-            callendar.classList.remove('callendar-unactive');
-            errorInputArrow.classList.remove('active');
-            errorInputInfo.classList.remove('active');
-            db.collection(uid)
-              .doc(`${inputHabbit.value}`)
-              .set({});
-            collectionSize(uid, inputHabbit.value);
-            checkTargetStatus(uid, inputHabbit.value);
-            habbitDisplay.innerHTML = inputHabbit.value;
-            db.collection(uid)
-              .doc('0000000')
-              .set({
-                lastHabbit: inputHabbit.value
-              });
-          });
-        });
-
-      checkIfUserLogged();
-    })
-    .catch(console.log);
-};
 
 progressBar();
 
